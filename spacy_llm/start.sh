052151bb8c9bd0bb442c3c7b50b1f3817e98ced9
Qiwei <101662556+BernieVA@users.noreply.github.com>
2024-03-26 16:42:54 -0600

    Update start.sh

--- spacy_llm/start.sh
+++ spacy_llm/start.sh
@@ -5,4 +5,9 @@ if [[ -f "/workspace/jupyter_server_config.json" ]]; then
 else
   echo "/workspace/jupyter_server_config.json does not exist. Will not copy to home directory." 
 fi
+export CUDA_VISIBLE_DEVICES=1 
+export HF_DATASETS_OFFLINE=1 
+export TRANSFORMERS_OFFLINE=1
+export HF_HUB_OFFLINE=1
+export HF_HOME=/workspace/pretrained_models/hub2
 jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root
